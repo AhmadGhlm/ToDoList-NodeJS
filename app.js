3bea99b931e8b7e61feb7e4a08d068e9c9e0786b
AhmadGDev <Ahmad.ghelem@gmail.com>
2019-11-07 12:31:42 +0100

    Move Bootstrape template HTML in the findAll request

--- app.js
+++ app.js
@@ -22,58 +22,56 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   Item.findAll().then(items => {
-    console.log(items);
-  });
-
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do App</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="display-4 text-center py-1">To-Do App</h1>
-      
-      <div class="jumbotron p-3 shadow-sm">
-        <form action="/" method="POST">
-          <div class="d-flex align-items-center">
-            <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-            <button class="btn btn-success">Ajouter la tâche</button>
-          </div>
-        </form>
+    res.send(`<!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Simple To-Do App</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+    <body>
+      <div class="container">
+        <h1 class="display-4 text-center py-1">To-Do App</h1>
+        
+        <div class="jumbotron p-3 shadow-sm">
+          <form action="/" method="POST">
+            <div class="d-flex align-items-center">
+              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <button class="btn btn-success">Ajouter la tâche</button>
+            </div>
+          </form>
+        </div>
+        
+        <ul class="list-group pb-5">
+          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">Aller faire des courses</span>
+            <div>
+              <button class="edit-me btn btn-warning btn-sm mr-1">Modifier</button>
+              <button class="delete-me btn btn-danger btn-sm">Supprimer</button>
+            </div>
+          </li>
+          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">Faire la session moto</span>
+            <div>
+              <button class="edit-me btn btn-warning btn-sm mr-1">Modifier</button>
+              <button class="delete-me btn btn-danger btn-sm">Supprimer</button>
+            </div>
+          </li>
+          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">Manger des gâteaux</span>
+            <div>
+              <button class="edit-me btn btn-warning btn-sm mr-1">Modifier</button>
+              <button class="delete-me btn btn-danger btn-sm">Supprimer</button>
+            </div>
+          </li>
+        </ul>
+        
       </div>
       
-      <ul class="list-group pb-5">
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Aller faire des courses</span>
-          <div>
-            <button class="edit-me btn btn-warning btn-sm mr-1">Modifier</button>
-            <button class="delete-me btn btn-danger btn-sm">Supprimer</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Faire la session moto</span>
-          <div>
-            <button class="edit-me btn btn-warning btn-sm mr-1">Modifier</button>
-            <button class="delete-me btn btn-danger btn-sm">Supprimer</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Manger des gâteaux</span>
-          <div>
-            <button class="edit-me btn btn-warning btn-sm mr-1">Modifier</button>
-            <button class="delete-me btn btn-danger btn-sm">Supprimer</button>
-          </div>
-        </li>
-      </ul>
-      
-    </div>
-    
-  </body>
-  </html>`);
+    </body>
+    </html>`);
+  });
 });
 
 app.post("/", (req, res) => {
